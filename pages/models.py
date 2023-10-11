59e9735d25917c172fb92474f610d3fe7bf7a04b
Mehran Mirzaei <mehranmirzaeiv@gmail.com>
2023-10-11 10:26:55 +0330

    added static files and blog app and slider model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,20 @@
+# Django Imports
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+# Locale Imports
+from utils import AbstractDateTime
 
-# Create your models here.
+
+class Slider(AbstractDateTime):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    link = models.URLField()
+    photo = models.ImageField()
+
+    class Meta:
+        verbose_name = _("Slide")
+        verbose_name_plural = _("Slides")
+        ordering = ("title",)
+
+    def __str__(self):
+        return f'{self.title} - {self.description}'
--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+# class Category(models.Model):
+#     title = models.CharField(max_length=100)
+#     slug = models.SlugField()
+#     parent = models.ForeignKey('self',on_delete=models.CASCADE,related_name='children')
+#
+#     class MPTTMeta:
+#         pass
+#
+#     class Meta:
+#         pass
+#
+#     def __str__(self):
+#         return self.title
